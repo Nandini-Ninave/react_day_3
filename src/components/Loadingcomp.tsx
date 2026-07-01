8c2dff64e87b2101dcb8c3a0f27d11966ca629c7
Nandini-Ninave <nandinininave99@gmail.com>
2026-07-01 19:06:39 +0530

    Revert "Added loader before api is fetched"
    This reverts commit 8ce2ff0781f626de103ae9e4b2cd67e0ae60282e.

--- src/components/Loadingcomp.tsx
+++ src/components/Loadingcomp.tsx
@@ -1,9 +1,9 @@
-import "./loadingcomp.css"
+// import "./loadingcomp.css"
 function Loadingcomp(){
     return(<div className="spinner">
         {/* <div className="animate-spin h-10 w-10 border-solid text-black
 rounded-4xl bg-black"></div> */}
-<div className="h-10px w-10px rounded-full outline-black load"></div>
+<div className="h-10px w-10px rounded-full outline-black"></div>
         </div>)
 }
 export default Loadingcomp
