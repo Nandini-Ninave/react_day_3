5cb0ec0e8d00b560d83aab84be1f83d260a268b4
Nandini-Ninave <nandinininave99@gmail.com>
2026-07-02 18:00:34 +0530

    Removed commented statements from aap.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,6 @@ function App(){
   const router = createBrowserRouter(routes)
   return(
     <RouterProvider router={router}></RouterProvider>)
-    // <div>
-      // <IncDec/>
-      {/* <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route element={<Protectedroute/>}>
-              <Route path="/about" element={<About/>}>
-                  <Route path="contact" element={<Contact/>}></Route>
-              <Route path="abouthome" element={<AboutHome/>}></Route>
-              </Route>
-            </Route>
-            <Route path="/person/:firstName" element={<Person/>}></Route>
-        </Routes>
-      </BrowserRouter> */}
-    // </div>
-  // )
 }
 export default App
 
